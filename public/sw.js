3bf0ff077aa27d2df051efe2a6bf4433a4d9a52f
okcawa0-a11y <okcawa0@gmail.com>
2026-08-30 18:02:15 +0700

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'notion-tkj-v1';
+const assetsToCache = [
+  '/',
+  '/style.css',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
